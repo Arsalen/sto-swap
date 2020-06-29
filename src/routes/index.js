e76d8d589c964f1917c2c05584d6e80aef9b9e3a
arsalen <arsalen.hagui@hotmail.com>
2020-06-29 03:59:26 +0100

    rpc provider

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,19 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Home 💎", MAGIC_PUBLISHABLE_KEY });
 });
 
+// GET DAI page
+router.get("/dai/", (req, res) => {
+  res.render("dai", { title: "DAI", MAGIC_PUBLISHABLE_KEY });
+});
+
+// GET USDC page
+router.get("/usdc/", (req, res) => {
+    res.render("usdc", { title: "USDC", MAGIC_PUBLISHABLE_KEY });
+});
+
+// GET USDT page
+router.get("/usdt/", (req, res) => {
+    res.render("usdt", { title: "USDT", MAGIC_PUBLISHABLE_KEY });
+});
+
 module.exports = router;
