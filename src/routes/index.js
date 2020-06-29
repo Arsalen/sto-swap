ab09bf55d772c2ef27f92f6ecb75d11022fe6a0f
arsalen <arsalen.hagui@hotmail.com>
2020-06-29 01:01:38 +0100

    user authentication

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+require('dotenv').config({path: '.env'});
+
+const MAGIC_PUBLISHABLE_KEY = process.env.MAGIC_PUBLISHABLE_KEY;
+
+// GET home page
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home 💎", MAGIC_PUBLISHABLE_KEY });
+});
+
+module.exports = router;
